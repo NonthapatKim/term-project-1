f4b44e89374e9b895bf7d5a1988609d795d0c230
Nonthapat Kimram <nonthapat.kimram@g.swu.ac.th>
2022-11-13 22:16:04 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="poy-sian" data-aos="fade-up">
+          <img :src="inp" class="d-block mx-auto img-fluid" alt="..." />
+        </div>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from "vue";
+import AOS from "aos";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  
+  data() {
+    return {
+      inp: require("./assets/h-1.png")
+    }
+  },
+
+  setup() {
+    onMounted(() => {
+      AOS.init();
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: rgb(214,70,77);
+  background: linear-gradient(90deg, rgba(214,70,77,1) 0%, rgba(105,159,203,1) 100%);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'aos/dist/aos.css'
+
 createApp(App).mount('#app')
+
+import 'bootstrap'
